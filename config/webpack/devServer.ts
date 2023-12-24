b13de862243405c12b83843d360292384961ae08
Timur Khrustalyov <hrustalevdev@gmail.com>
2023-12-24 21:48:30 +0300

    Update `devServer` config

--- config/webpack/devServer.ts
+++ config/webpack/devServer.ts
@@ -5,7 +5,9 @@ import type { IBuildOptions } from './types/config';
 export const getDevServerConfig = ({port}: IBuildOptions): DevServerConfig => {
   return {
     port,
-    /** Будет автоматически открывать в браузере приложение */
+    /** Будет автоматически открывать в браузере приложение. */
     open: false,
+    /** Для работы с роутингом в SPA. */
+    historyApiFallback: false,
   }
 }
