bf40431bbb946f868187cb32717aa99cb6bc8497
Timur Khrustalyov <hrustalevdev@gmail.com>
2023-12-28 14:29:02 +0300

    Update `global.d.ts`

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -6,3 +6,24 @@ declare module "*.scss" {
   const classNames: IClassNames;
   export = classNames;
 }
+
+declare module '*.svg' {
+  import React from 'react';
+  const SVGComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export = SVGComponent;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export = content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export = content;
+}
+
+declare module "*.json" {
+  const content: string;
+  export = content;
+}
