ec24ececafdbe97cb0611d1e060ac4a0a1fd628c
Timur Khrustalyov <hrustalevdev@gmail.com>
2023-12-28 15:09:06 +0300

    Update `Default` layout

--- src/app/layouts/Default.tsx
+++ src/app/layouts/Default.tsx
@@ -1,19 +1,16 @@
 import { cn } from 'shared/lib/classNames';
 import { Navbar } from 'widgets/Navbar';
-import { useTheme } from '../providers/ThemeProvider';
+import { useTheme } from '../providers';
 import { Router } from '../router';
 
 import '../styles/index.scss';
 
 export const Default = () => {
-  const {theme, toggleTheme} = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={cn('app', theme)}>
       <Navbar />
-      <div>
-        <button onClick={toggleTheme}>TOGGLE THEME</button>
-      </div>
       <Router />
     </div>
   )
